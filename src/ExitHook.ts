dc433f6cf107f1a5c4c186a8abee270a8110e764
ducktrshessami <jwee761@gmail.com>
2023-02-25 14:57:11 -0800

    base exit task flow

--- src/ExitHook.ts
+++ src/ExitHook.ts
@@ -2,22 +2,41 @@ import { schedule, ScheduledTask } from "node-cron";
 
 export default class ExitHook {
     private readonly options: ParsedExitHookOptions;
+    private _active: boolean;
     private job: ScheduledTask | null;
 
     constructor(cronExpression: string, options: ExitHookOptions) {
         this.options = ExitHook.parseOptions(options);
+        this._active = this.options.active;
         this.job = schedule(cronExpression, this.task, { scheduled: this.options.active });
     }
 
     private static parseOptions(options: ExitHookOptions): ParsedExitHookOptions {
         return {
+            ...options,
             active: options.active ?? true,
             exitCode: options.exitCode ?? 0
         };
     }
 
-    private task(): void {
-        // exit
+    private async exit(): Promise<void> {
+        if (this.options.beforeExit) {
+            await this.options.beforeExit();
+        }
+        process.exit(this.options.exitCode);
+    }
+
+    private async task(): Promise<void> {
+        if (this._active) {
+            await this.exit();
+        }
+        else {
+            // timeouts
+        }
+    }
+
+    get active(): boolean {
+        return this._active;
     }
 
     get destroyed(): boolean {
@@ -32,12 +51,18 @@ export default class ExitHook {
     }
 }
 
+type Awaitable<T> = T | Promise<T>;
+
+type BeforeExitHook = () => Awaitable<void>;
+
 export type ExitHookOptions = {
     active?: boolean;
     exitCode?: number;
+    beforeExit?: BeforeExitHook
 };
 
 type ParsedExitHookOptions = {
     active: boolean;
     exitCode: number;
+    beforeExit?: BeforeExitHook
 };
