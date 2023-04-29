c0fa9f31e412c1777675c437848ac50e7e421947
ducktrshessami <ducktrshessami@users.noreply.github.com>
2023-04-29 03:43:08 +0000

    Build 6568bc872e1dd02d3d9b51d788df06b62b3733eb

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,31 +1,37 @@
 declare class ExitHook$1 {
-    private readonly options;
+    readonly cronExpression: string;
+    readonly options: Readonly<ParsedExitHookOptions>;
     private _active;
     private job;
     private jobComplete;
     private restartTimeout;
     private maxTimeout;
     constructor(cronExpression: string, options: ExitHookOptions);
+    get active(): boolean;
+    get destroyed(): boolean;
     private static parseOptions;
+    private logVerbose;
     private clearRestartTimeout;
     private clearTimeouts;
     private exit;
     private task;
-    get active(): boolean;
-    get destroyed(): boolean;
     destroy(): void;
     start(): void;
     stop(): void;
 }
 type Awaitable<T> = T | Promise<T>;
 type BeforeExitHook = () => Awaitable<void>;
-type ExitHookOptions = {
-    restartDelay?: number;
+type ParsedExitHookOptions = {
+    verbose: boolean;
+    restartDelay: number;
     maxDelay?: number;
-    active?: boolean;
+    active: boolean;
     beforeExit?: BeforeExitHook;
-    exitCode?: number;
-    errorExitCode?: number;
+    exitCode: number;
+    errorExitCode: number;
+};
+type ExitHookOptions = {
+    [key in keyof ParsedExitHookOptions]?: ParsedExitHookOptions[key];
 };
 
 declare function exitHook(cronExpression: string, options?: ExitHookOptions): ExitHook;
